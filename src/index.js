7fd59cbb30e92d89712562c80c6caa8d8adeb6b4
DanilZen <a64danil@mail.ru>
2021-03-09 16:45:13 +0300

    test husky

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-import './styles/main.scss';
+import './styles/main.scss'
 
-console.log('entry point');
+console.log('entry point')
 
 // создание свойства класса без конструктора
 class Game {
-    name = 'Violin Charades'
+  // name = 'Violin Charades'
+  // name: 'Violin Charades'
 }
 const myGame = new Game()
 
-console.log(myGame);
+// console.log(myGame);
 
+console.log('12-33')
 
-console.log('12-33');
+function bad(a) {
+  console.log('ha h')
+  return a + 2
+}
+
+const testObj = {
+  one: 'odin',
+  two: 'dva',
+  three: 'tri',
+}
+
+for (const key of testObj) {
+  console.log(key + ':' + testObj[key]) // original
+  console.log(`${key  }:${  testObj[key]}`) // fixed by eslint
+}
+
+const one = testObj.one;
+
+console.log(one)
