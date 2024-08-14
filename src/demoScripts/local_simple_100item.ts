cf150dab216de7e4a06934f5bb68ff8ccc0e4b53
DanilZen <a64danil@mail.ru>
2024-08-14 11:48:36 +0200

    change demo height for tests

--- src/demoScripts/local_simple_100item.ts
+++ src/demoScripts/local_simple_100item.ts
@@ -6,7 +6,7 @@ const BigJson1 = BigDataList100.data;
 export const LOCAL_SIMPLE_100ITEMS_PROPS: InfinityScrollPropTypes = {
   data: BigJson1,
   selectorId: 'LOCAL_SIMPLE_100ITEMS',
-  listWrapperHeight: '350px',
+  listWrapperHeight: '270px',
   templateString: (element, listLength, number) => `<li
         class="LOCAL_SIMPLE_100ITEMS_List__listItem" >
             ${element.name} ${element.number} - ${number}
