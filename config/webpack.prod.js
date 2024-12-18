107191f92d2890021ec7fb96ec2b7e74ea3f494a
DanilZen <a64danil@mail.ru>
2024-12-18 21:43:27 +0100

    add more options for OUTPUT file in prod.mode, like libraryExport: 'InfinityScroll'

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -8,12 +8,15 @@ const path = require('path')
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
+// TODO: жесть, почему из-за такого пути CleanWebpack плагин ругается????
+// const cssEntryPoints = glob.sync('./src/styles/demo/*.scss').reduce((acc, file) => {
 const cssEntryPoints = glob.sync('./src/styles/**/*.scss').reduce((acc, file) => {
     const name = path.basename(file, '.scss'); // Имя файла без расширения
     acc[name] = file; // Добавляем SCSS-файлы как entry points
     return acc;
 }, {});
 
+console.log(cssEntryPoints)
 
 
 const cssConfig = {
@@ -72,7 +75,12 @@ const mainProdConfig = merge(common, {
     output: {
         path: paths.dist,
         publicPath: '',
-        filename: 'js/[name].[contenthash].bundle.js',
+        // filename: 'js/[name].[contenthash].bundle.js',
+        filename: 'js/[name].bundle.js',
+        library: 'InfinityScroll', // Имя, под которым будет доступен класс
+        libraryTarget: 'global', // Указываем, что класс добавляется в глобальный объект (window)
+        // libraryTarget: 'window', // Указываем, что класс добавляется в глобальный объект (window)
+        libraryExport: 'InfinityScroll', // Экспортирует конкретное свойство, а не весь объект
     },
     optimization: {
         // minimize: true,
@@ -83,9 +91,10 @@ const mainProdConfig = merge(common, {
                 new TerserPlugin({
                     terserOptions: {
                         compress: {
-                            pure_funcs: ['console.log'], // Удалить только console.log
+                            // pure_funcs: ['console.log'], // Удалить только console.log
                             // drop_console: true, // Удалить все консоли
                         },
+                        keep_classnames: /^InfinityScroll$/, // Сохраняем конкретное имя класса
                     },
                 }).apply(compiler);
             },
@@ -94,9 +103,10 @@ const mainProdConfig = merge(common, {
         // Once your build outputs multiple chunks, this option will ensure they share the webpack runtime
         // instead of having their own. This also helps with long-term caching, since the chunks will only
         // change when actual code changes, not the webpack runtime.
-        runtimeChunk: {
-            name: 'runtime',
-        },
+        // runtimeChunk: {
+        //     name: 'runtime',
+        // },
+        runtimeChunk: false,
     },
     performance: {
         hints: false,
