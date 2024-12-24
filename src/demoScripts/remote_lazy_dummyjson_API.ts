dfd400b851b7228c8b6db514e91f9404f8415cce
DanilZen <a64danil@mail.ru>
2024-12-24 23:20:09 +0100

    add files to have DUMMY_JSON demo separated, in single file

---
+++ src/demoScripts/remote_lazy_dummyjson_API.ts
@@ -0,0 +1,15 @@
+import { InfinityScrollPropTypes } from '../js/types/InfinityScrollPropTypes';
+
+export const REMOTE_LAZY_DUMMYJSON_API_PROPS: InfinityScrollPropTypes = {
+  data: ({ start, end, page, limit }) =>
+    `https://dummyjson.com/products?limit=${limit}&skip=${start}`,
+  selectorId: 'REMOTE_LAZY_DUMMYJSON_API',
+  subDir: 'products',
+  listType: 'list',
+  listWrapperHeight: '290px',
+  templateString: (element, listLength) => `<li 
+        class="REMOTE_LAZY_DUMMYJSON_API_List__listItem big" 
+        >
+            ${element?.id} ${element?.title}
+    </li>`,
+};
