91778ba658848b6b8a3b7f0f1f9ef9ec83d1da79
DanilZen <a64danil@mail.ru>
2024-12-28 17:09:27 +0100

    update lazy_API_100items demo

--- src/demoScripts/remote_lazy_API_100items.ts
+++ src/demoScripts/remote_lazy_API_100items.ts
@@ -5,7 +5,6 @@ export const REMOTE_LAZY_API_PROPS_100ITEMS: InfinityScrollPropTypes = {
     `https://restapi.qoobeo.ru/api/v1/companys?start=${start}&end=${end}`,
   selectorId: 'REMOTE_LAZY_API_100ITEMS',
   forcedListLength: 100,
-  listWrapperHeight: '290px',
   templateString: (element, listLength, elemNum) => `<li 
         class="REMOTE_LAZY_API__listItem" 
         >
