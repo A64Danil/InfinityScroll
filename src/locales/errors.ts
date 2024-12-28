ae0c7e75f3b51b30ca9216ba45a4e844450eda73
DanilZen <a64danil@mail.ru>
2024-12-28 16:55:09 +0100

    add LOCALES(errors)

---
+++ src/locales/errors.ts
@@ -0,0 +1,10 @@
+export const errorMsg: Record<string, any> = {
+  en: {
+    zeroHeight:
+      'Your list height is less than 10px. You need to set your list-height from STYLES (by css) or pass like property in JS',
+  },
+  ru: {
+    zeroHeight:
+      'Высота вашего списка меньше чем 10px. Вам нужно установить высоту вашего списка из СТИЛЕЙ (через CSS) или передавать как свойство в JS',
+  },
+};
