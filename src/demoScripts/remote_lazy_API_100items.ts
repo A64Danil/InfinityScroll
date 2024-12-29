c66da1a4d5c0e80576d0b307f8f8fd3ee5cb5612
DanilZen <a64danil@mail.ru>
2024-12-29 12:24:48 +0100

    update demo -> change listType to LIST in lazy_api_100items

--- src/demoScripts/remote_lazy_API_100items.ts
+++ src/demoScripts/remote_lazy_API_100items.ts
@@ -2,12 +2,18 @@ import { InfinityScrollPropTypes } from '../js/types/InfinityScrollPropTypes';
 
 export const REMOTE_LAZY_API_PROPS_100ITEMS: InfinityScrollPropTypes = {
   data: ({ start, end }): string =>
-    `https://restapi.qoobeo.ru/api/v1/companys?start=${start}&end=${end}`,
+    `https://restapi.qoobeo.ru/api/v1/companys?start=${start}&end=${end}&isFullSchema=true`,
   selectorId: 'REMOTE_LAZY_API_100ITEMS',
   forcedListLength: 100,
-  templateString: (element, listLength, elemNum) => `<li 
+  listType: 'table',
+  templateString: (element, listLength, elemNum) => `<tr
         class="REMOTE_LAZY_API__listItem" 
         >
-           ${element?.name} (elemNum - ${elemNum})
-    </li>`,
+          <td>#${elemNum} - id${element?.id}</td>
+          <td>${element?.name} (${element?.country_code})</td>
+          <td>${element?.industry}</td>
+          <td>${element?.founded}</td>
+          <td>${element?.size}</td>
+          <td>${element?.city} (${element?.state})</td>
+    </tr>`,
 };
