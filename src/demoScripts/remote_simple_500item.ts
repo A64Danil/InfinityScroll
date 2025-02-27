88488c7be13503933440d471b95fca6698120bed
DanilZen <a64danil@mail.ru>
2025-02-27 16:31:30 +0100

    update simple_500item list demo

--- src/demoScripts/remote_simple_500item.ts
+++ src/demoScripts/remote_simple_500item.ts
@@ -2,13 +2,15 @@ import { InfinityScrollPropTypes } from '../js/types/InfinityScrollPropTypes';
 
 export const REMOTE_SIMPLE_500ITEMS_PROPS: InfinityScrollPropTypes = {
   data: 'https://jsonplaceholder.typicode.com/comments',
-  name: 'my REMOTE_SIMPLE_500ITEMS',
   selectorId: 'REMOTE_SIMPLE_500ITEM',
   listType: 'list',
-  listWrapperHeight: '290px',
+  listWrapperHeight: '590px',
   templateString: (element, listLength, elemNum) => `<li 
         class="REMOTE_SIMPLE_500ITEM_List__listItem" 
         >
-            ${element.email} ${element.id} (elemNum - ${elemNum})
+            <div class="listItemContainer">
+             <p class="header"><em>User:</em> ${element.email} <span>(user.id - ${element.id})</span></p>
+             <p class="descr"><em>Text:</em> ${element.body} </p>
+           </div>
     </li>`,
 };
