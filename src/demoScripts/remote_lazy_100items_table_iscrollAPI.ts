15e7f912c6cd603f73ba219d069e004588930756
DanilZen <a64danil@mail.ru>
2025-03-03 17:10:55 +0100

    update name of the TABLE demo

--- src/demoScripts/remote_lazy_100items_table_iscrollAPI.ts
+++ src/demoScripts/remote_lazy_100items_table_iscrollAPI.ts
@@ -1,14 +1,14 @@
 import { InfinityScrollPropTypes } from '../js/types/InfinityScrollPropTypes';
 
-export const REMOTE_LAZY_API_PROPS_100ITEMS: InfinityScrollPropTypes = {
+export const REMOTE_LAZY_100ITEMS_TABLE_ISCROLLAPI_PROPS: InfinityScrollPropTypes = {
   data: ({ start, end }): string =>
     `https://restapi.qoobeo.ru/api/v1/companys?start=${start}&end=${end}&isFullSchema=true`,
-  selectorId: 'REMOTE_LAZY_API_100ITEMS',
+  selectorId: 'REMOTE_LAZY_100ITEMS_TABLE_ISCROLLAPI',
   forcedListLength: 100,
   listType: 'table',
   tHeadNames: ['id', 'name', 'industry', 'founded', 'size', 'city (state)'],
   templateString: (element, listLength, elemNum) => `<tr
-        class="REMOTE_LAZY_API_100ITEMS__listItem" 
+        class="REMOTE_LAZY_100ITEMS_TABLE_ISCROLLAPI__listItem" 
         >
           <td>${element?.id}</td>
           <td>${element?.name} (${element?.country_code})</td>
