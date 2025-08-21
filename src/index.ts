28961be333b80a417d3c085ef799c602ef7fcb4e
DanilZen <a64danil@mail.ru>
2025-08-21 15:34:20 +0200

    refator: start of all demos (in dev mode)

--- src/index.ts
+++ src/index.ts
@@ -25,103 +25,34 @@ console.log('Entry point');
 
   await loadDevStyles();
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const BigJson1 = BigDataList100.data;
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  // const BigJson2 = BigDataList10k.data;
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  // const BigJson3 = BigDataList100k.data;
+  window.iScroll = [] as InfinityScroll[];
 
-  const pureList = document.getElementById(
-    LOCAL_BASIC_10000ITEMS_PROPS.selectorId
-  );
+  const listElements = [
+    LOCAL_BASIC_10000ITEMS_PROPS,
+    LOCAL_SIMPLE_100ITEMS_PROPS,
+    REMOTE_SIMPLE_500ITEMS_PROPS,
+    REMOTE_SIMPLE_API_100ITEMS_PROPS,
+    REMOTE_SIMPLE_15ITEMS_DUMMYJSON_API_PROPS,
+    REMOTE_LAZY_DUMMYJSON_API_PROPS,
+    REMOTE_LAZY_API_PROPS,
+    REMOTE_LAZY_100ITEMS_TABLE_ISCROLLAPI_PROPS,
+    REMOTE_LAZY_5KK_ITEMS_TABLE_ISCROLLAPI_PROPS,
+  ].map((props) => {
+    const el = document.getElementById(props.selectorId);
+    if (el !== null) {
+      return {
+        props,
+        instance: new InfinityScroll(props),
+      };
+    }
+    return null;
+  }).filter(Boolean);
 
-  if (pureList !== null) {
-    console.log('Basic list Started');
-    const myLazyScroll = new InfinityScroll(LOCAL_BASIC_10000ITEMS_PROPS);
-    window.iScroll = myLazyScroll;
-  }
-
-  const instantList = document.getElementById(
-    LOCAL_SIMPLE_100ITEMS_PROPS.selectorId
-  );
-  if (instantList !== null) {
-    console.log('Instant list Started');
-    const myInstantScroll = new InfinityScroll(LOCAL_SIMPLE_100ITEMS_PROPS);
-    window.iScroll = myInstantScroll;
-  }
-
-  const remoteList = document.getElementById(
-    REMOTE_SIMPLE_500ITEMS_PROPS.selectorId
-  );
-
-  if (remoteList !== null) {
-    console.log('Remote list Started');
-    const myLazyScroll = new InfinityScroll(REMOTE_SIMPLE_500ITEMS_PROPS);
-    window.iScroll = myLazyScroll;
-  }
-
-  const remoteListApi = document.getElementById(
-    REMOTE_SIMPLE_API_100ITEMS_PROPS.selectorId
-  );
-
-  if (remoteListApi !== null) {
-    console.log('Remote Api_100_items list Started');
-    const myLazyScroll = new InfinityScroll(REMOTE_SIMPLE_API_100ITEMS_PROPS);
-    window.iScroll = myLazyScroll;
-  }
-
-  const simpleDummyJsonList = document.getElementById(
-    REMOTE_SIMPLE_15ITEMS_DUMMYJSON_API_PROPS.selectorId
-  );
-
-  if (simpleDummyJsonList !== null) {
-    console.log('Simle DummyJson list Started');
-    const mySimpleScroll = new InfinityScroll(REMOTE_SIMPLE_15ITEMS_DUMMYJSON_API_PROPS);
-    window.iScroll = mySimpleScroll;
-  }
-
-  const lazyDummyJsonList = document.getElementById(
-    REMOTE_LAZY_DUMMYJSON_API_PROPS.selectorId
-  );
-
-  if (lazyDummyJsonList !== null) {
-    console.log('Lazy DummyJson list Started');
-    const myLazyScroll = new InfinityScroll(REMOTE_LAZY_DUMMYJSON_API_PROPS);
-    window.iScroll = myLazyScroll;
-  }
-
-  const lazyAPIList = document.getElementById(REMOTE_LAZY_API_PROPS.selectorId);
-
-  if (lazyAPIList !== null) {
-    console.log('Lazy API list Started');
-    const myLazyAPIScroll = new InfinityScroll(REMOTE_LAZY_API_PROPS);
-    window.iScroll = myLazyAPIScroll;
-  }
-
-  const lazyAPITable100 = document.getElementById(
-      REMOTE_LAZY_100ITEMS_TABLE_ISCROLLAPI_PROPS.selectorId
-  );
-
-  if (lazyAPITable100 !== null) {
-    console.log('Lazy API list Started');
-    const myLazyAPIScroll100 = new InfinityScroll(
-        REMOTE_LAZY_100ITEMS_TABLE_ISCROLLAPI_PROPS
-    );
-    window.iScroll = myLazyAPIScroll100;
-  }
-
-  const lazyAPITable5kk = document.getElementById(
-      REMOTE_LAZY_5KK_ITEMS_TABLE_ISCROLLAPI_PROPS.selectorId
-  );
-
-  if (lazyAPITable5kk !== null) {
-    console.log('Lazy API Table 5kk Started');
-    const myLazyAPITable5kk = new InfinityScroll(
-        REMOTE_LAZY_5KK_ITEMS_TABLE_ISCROLLAPI_PROPS
-    );
-    window.iScroll = myLazyAPITable5kk;
-  }
+  listElements.forEach(({ props, instance }) => {
+    const name = props.selectorId.replaceAll('_', ' ').toLowerCase();
+    console.log(`"${name}" list started!`);
+    window.iScroll.push(instance);
+  });
 
   const StartBtn: HTMLElement | null = document.querySelector<HTMLElement>(
     '#infinityScrollListStartBtn'
