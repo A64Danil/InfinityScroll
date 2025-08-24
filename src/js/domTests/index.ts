46495df095f27991b7b916f3e9bde97672ce4c51
DanilZen <a64danil@mail.ru>
2025-08-24 17:05:58 +0200

    fix: now listName have a device type additionaly

--- src/js/domTests/index.ts
+++ src/js/domTests/index.ts
@@ -9,6 +9,7 @@ import { DOMTest } from './testFunctions';
 
 export async function iScrollTester(): Promise<object> {
   this.testResults.errors.clear();
+  this.testResults.listName = `${this.testResults.listName} (${this.vsb.deviceType})`;
 
   console.log('iScrollTester log msg');
 
