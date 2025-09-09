9ae96950ab3f0fea002ea66480623601cb2f9dd3
DanilZen <a64danil@mail.ru>
2025-09-09 11:43:11 +0200

    style: remove tabs

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    plugins: {
-        'postcss-preset-env': {
-            browsers: 'last 2 versions',
-            // browsers: '> 0.1%',
-        },
+  plugins: {
+    'postcss-preset-env': {
+      browsers: 'last 2 versions',
+      // browsers: '> 0.1%',
     },
-}
+  },
+};
